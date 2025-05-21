ed689812c0a490f2d87a5df1597ab050b568cf58
Aran Ahmed <aranahmed1@live.co.uk>
2025-05-21 22:32:06 +0100

    Add files via upload

---
+++ PlacedBySplineObjectPlacer.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlacedBySplineObjectPlacer : MonoBehaviour
+{
+    public string toolIdentifier = "SplineObjectPlacer";
+    public int placementIndex;
+}
+
